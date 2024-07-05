af15fab416ad79c5de0b13448c330c197e5099b5
General-Meow <spl.ivalis@gmail.com>
2024-07-05 10:59:06 +0100

    error page fix

--- src/ErrorPage.js
+++ src/ErrorPage.js
@@ -1,7 +1,25 @@
+import errorImage from './img.png';
+import Typography from "@mui/material/Typography";
+import './ErrorPage.css';
+import {useRouteError} from "react-router";
+
 const ErrorPage = props => {
+    let errorObj = useRouteError();
+    console.error("error: ", errorObj.error.message);
     return (
-        <div>
-            <img src='./img.png'/>
+        <div class='ErrorComponent'>
+            <Typography variant="h3" component="div" className='errorHeader'>
+                Uhoh, something broke
+            </Typography>
+            <main>
+                <span class='element'>
+                    <img src={errorImage} alt='error image' className='errorImage'/>
+                </span>
+                <span class='element'>
+                    {errorObj.error.message}
+                </span>
+            </main>
+
         </div>
     )
 }
